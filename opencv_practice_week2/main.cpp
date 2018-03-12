4d31189941f2a8aa9c6ab5697e9748d4fc6a6fbc
류예나 <yeahna@lyuyenaui-macbook-pro.local>
2018-03-12 13:57:08 +0900

    Initial Commit

---
+++ opencv_practice_week2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opencv_practice_week2
+//
+//  Created by 류예나 on 2018. 3. 12..
+//  Copyright © 2018년 류예나. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
